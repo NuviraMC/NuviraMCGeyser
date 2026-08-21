36bda6a8ca04ba2721d4b8ae2852626f35f6f371
larroxtv <devlarrox@gmail.com>
2026-08-21 22:42:34 +0200

    Added some test values just for testing purposes

--- src/main/java/net/nuviramc/nuviraMCGeyser/util/SimpleFormBuilder.java
+++ src/main/java/net/nuviramc/nuviraMCGeyser/util/SimpleFormBuilder.java
@@ -1,34 +1,57 @@
 package net.nuviramc.nuviraMCGeyser.util;
 
 import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.geysermc.cumulus.form.SimpleForm;
 import org.geysermc.cumulus.util.FormImage;
+import org.geysermc.floodgate.api.FloodgateApi;
 
-import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SimpleFormBuilder {
 
-    private static SimpleFormBuilder instance;
+    public void openServerList(Player viewer) {
+        List<Player> onlinePlayers = new ArrayList<>(Bukkit.getOnlinePlayers());
 
-    public static SimpleFormBuilder getInstance() {
-        if (instance == null) {
-            instance = new SimpleFormBuilder();
-        }
-        return instance;
-    }
-
-    public SimpleForm initServerList() {
         SimpleForm.Builder form = SimpleForm.builder()
                 .title("Serverliste - NuviraMC")
-                .content("Es sind derzeit " + Bukkit.getServer().getOnlinePlayers().size() + " Spieler online!");
+                .content("Es sind derzeit " + onlinePlayers.size() + " Spieler online!");
 
-        for (Player player : Bukkit.getOnlinePlayers()) {
+        for (Player player : onlinePlayers) {
             form.button(player.getName(), FormImage.Type.URL,
                     ConfigLoader.getSkinApiUrl() + player.getName());
         }
-        return form.build();
+
+        form.validResultHandler(response -> {
+            int clickedIndex = response.clickedButtonId();
+            if (clickedIndex >= 0 && clickedIndex < onlinePlayers.size()) {
+                Player target = onlinePlayers.get(clickedIndex);
+                openPlayerStats(viewer, target);
+            }
+        });
+
+        FloodgateApi.getInstance().sendForm(viewer.getUniqueId(), form.build());
+    }
+
+    public void openPlayerStats(Player viewer, Player target) {
+        SimpleForm.Builder form = SimpleForm.builder()
+                .title("Statistiken - " + target.getName())
+                .content(buildStatsContent(target))
+                .button("Zurück");
+
+        form.validResultHandler(response -> {
+            if (response.clickedButtonId() == 0) {
+                openServerList(viewer);
+            }
+        });
+
+        FloodgateApi.getInstance().sendForm(viewer.getUniqueId(), form.build());
+    }
+
+    private String buildStatsContent(Player target) {
+        return "Level: " + target.getLevel() + "\n"
+                + "Health: " + target.getHealth() + "\n"
+                + "Ping: " + target.getPing() + "ms";
     }
 }
